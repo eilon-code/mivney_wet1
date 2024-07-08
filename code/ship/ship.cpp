7f5c91134e4e93358e229bd7d0c387e1188c35a0
eilon-code <eilon.halevy12@gmail.com>
2024-07-08 13:32:26 +0300

    implemented easy function

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -8,3 +8,13 @@ StatusType Ship::removePirate(int pirateId)
 StatusType Ship::insertPirate(const Pirate &pirate)
 {
 }
+
+StatusType Ship::updateRichestPirate()
+{
+    output_t<Pirate&> result = m_piratesOnShipOrderedByRichness.getMax();
+    if (result.status() != StatusType::SUCCESS) {
+        return result.status();
+    }
+    Pirate& richestPirate = result.ans();
+    m_richestPirate = &richestPirate;
+}
