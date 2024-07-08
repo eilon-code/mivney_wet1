47f06a091af308e5f92489f24aa9136f38be26df
eilon-code <eilon.halevy12@gmail.com>
2024-07-08 14:19:32 +0300

    implemented changePirateTreasure!

--- code/ship/ship.cpp
+++ code/ship/ship.cpp
@@ -5,6 +5,25 @@ StatusType Ship::removePirate(int pirateId)
     return StatusType();
 }
 
+StatusType Ship::changePirateTreasure(int pirateId, int change)
+ {
+    output_t<PirateRank&> searchResult = m_piratesOnShipOrderedByRichness.get(pirateId);
+    if (searchResult.status() != StatusType::SUCCESS) {
+        return searchResult.status();
+    }
+    PirateRank pirateRank = searchResult.ans();
+    Pirate* piratePointer = pirateRank.getPiratePointer();
+
+    StatusType result = m_piratesOnShipOrderedByRichness.remove(pirateId);
+    if (result != StatusType::SUCCESS) {
+        return result;
+    }
+    piratePointer->updateTreasure(change);
+    PirateRank pirateRankNew = PirateRank(piratePointer);
+    m_piratesOnShipOrderedByRichness.insert(pirateRankNew); // should work, because deletion of the same object worked
+    return StatusType::SUCCESS;
+}
+
 StatusType Ship::removeVeteranPirate()
 {
     output_t<Pirate&> result = findVeteranPirate();
