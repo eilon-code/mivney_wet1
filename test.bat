1d08150a2d624604626f74d9a42b142f6f47eeb5
eilon-code <eilon.halevy12@gmail.com>
2024-07-10 01:18:47 +0300

    the tests + a dynamic test of mine

---
+++ test.bat
@@ -0,0 +1,34 @@
+@echo off
+
+rem Clean previous builds
+echo Cleaning previous builds...
+mingw32-make -f makefile clean
+
+rem Compile the program
+echo Compiling the program...
+mingw32-make -f makefile
+
+@REM rem Check if the input file exists
+@REM if exist ./tests/test1.in (
+@REM     echo Input file found: tests/test1.in
+@REM     type nul > test.out
+@REM ) else (
+@REM     echo Input file not found: tests/test1.in
+@REM )
+
+@REM if exist test.out (
+@REM     echo Output file found: test.out
+@REM ) else (
+@REM     echo Input file not found: test.out
+@REM )
+
+rem Check if the executable was created
+if exist ./build/prog.exe (
+    echo Executable found, running the program...
+    .\build\prog.exe < ./tests/test1.in > test.out
+) else (
+    echo Executable not found. Compilation might have failed.
+)
+
+rem Pause to see the output in the command prompt
+@REM pause
