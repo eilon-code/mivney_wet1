4a0f80bb6822cd6fb9448d7be52b488e535ac61f
eilon-code <eilon.halevy12@gmail.com>
2024-07-17 22:09:50 +0300

    wong's fix

--- code/pirates24b1.cpp
+++ code/pirates24b1.cpp
@@ -80,7 +80,7 @@ StatusType Ocean::remove_pirate(int pirateId)
 
 StatusType Ocean::treason(int sourceShipId, int destShipId)
 {
-    if (sourceShipId <= 0 || destShipId <= 0) {
+    if (sourceShipId <= 0 || destShipId <= 0 || sourceShipId == destShipId) {
         return StatusType::INVALID_INPUT;
     }
 
